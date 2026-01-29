0dae4e800016ff1564c80176449e97f22f098e7f
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-29 22:19:13 +0530

    feat: Spy Number

---
+++ src/com/java/programming/level3/SpyNumber.java
@@ -0,0 +1,24 @@
+package com.java.programming.level3;
+
+import java.util.Scanner;
+
+public class SpyNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = sc.nextInt();
+        int sum = 0;
+        int product = 1;
+        while (n != 0) {
+            int temp = n % 10;
+            sum += temp;
+            product *= temp;
+            n = n / 10;
+
+            if (sum == product) {
+                System.out.println("The given number is spy number ");
+            }
+        }
+
+    }
+}
