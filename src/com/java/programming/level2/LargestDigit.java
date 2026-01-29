e12a116d8d5a1c9273a57d28eb28e51178dc5085
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-29 22:08:35 +0530

    feat:Largest digit of a numbers

---
+++ src/com/java/programming/level2/LargestDigit.java
@@ -0,0 +1,22 @@
+package com.java.programming.level2;
+
+import java.util.Scanner;
+
+public class LargestDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number:");
+        int n = sc.nextInt();
+        sc.close();
+        int largestDigit = 0;
+
+        while (n != 0) {
+            int temp = n % 10;
+            if (temp > largestDigit) {
+                largestDigit = temp;
+            }
+            n = n / 10;
+        }
+        System.out.println("Largest digit is :" + largestDigit);
+    }
+}
