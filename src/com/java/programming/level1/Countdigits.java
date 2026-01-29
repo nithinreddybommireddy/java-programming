d55f74798abc3f82b13385f47a516ff44128eed7
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-29 14:11:43 +0530

    feat:count digits

---
+++ src/com/java/programming/level1/Countdigits.java
@@ -0,0 +1,25 @@
+package com.java.programming.level1;
+
+import java.util.Scanner;
+
+public class Countdigits {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number:");
+        int n=sc.nextInt();
+        System.out.println("Digits on given number");
+          int count=1;
+        for(int i=1;i<n;i++)
+        {
+          int s=n%10;
+          count++;
+         n=n/10;
+        }
+        System.out.println(count);
+
+
+    }
+
+
+}
