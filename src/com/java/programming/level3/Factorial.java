c6182121633090489c13ad1bcc57005f9d6a75d2
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-29 22:15:44 +0530

    feat:Factorial of a Number

---
+++ src/com/java/programming/level3/Factorial.java
@@ -0,0 +1,17 @@
+package com.java.programming.level3;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        System.out.println("factorial of given num is :" + f);
+    }
+}
