3a28f437da8d8fdc7b6df33b14de019aaa2f2838
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-30 10:05:08 +0530

    feat:Strong Number

--- src/com/java/programming/level3/StrongNum.java
+++ src/com/java/programming/level3/StrongNum.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class StrongNum {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter the number : ");
         int number = input.nextInt();
         int sum = 0;
         int temp = number;
 
-        while(number > 0) {
-            int s = number%10;
-            int factorial =1;
-             for(int j=1;j<=s;j++){
-                 factorial = factorial*j;
-             }
-            sum = sum + factorial;
-            number = number/10;
-
+        while (number > 0) {
+            int s = number % 10;
+            int factorial = 1;
+            for (int j = 1; j <= s; j++) {
+                factorial = factorial * j;
             }
+            sum = sum + factorial;
+            number = number / 10;
+        }
         if (sum == temp) {
             System.out.println("The number is a strong number.");
-        }else {
+        } else {
             System.out.println("The number is not a strong number.");
         }
 
