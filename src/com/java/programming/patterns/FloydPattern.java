1244df41635d5fd3356303a76dc2f470c55ad0fc
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-31 15:13:10 +0530

    feat:Floyd pattern..

---
+++ src/com/java/programming/patterns/FloydPattern.java
@@ -0,0 +1,26 @@
+package com.java.programming.patterns;
+
+import java.util.Scanner;
+
+public class FloydPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements:");
+        int n = sc.nextInt();
+        int f=1;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j<n-i+1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < i ; k++) {
+                System.out.print(f + " ");
+                f++;
+            }
+            System.out.println();
+
+        }
+    }
+
+
+}
