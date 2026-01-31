d46cb2ba88ef18e92575b31c404dfe2430db0e4f
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-31 15:13:51 +0530

    feat: Stars pattern

---
+++ src/com/java/programming/patterns/StarPattern.java
@@ -0,0 +1,28 @@
+package com.java.programming.patterns;
+
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N:");
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j < n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            for (int k = 1; k <=i*2; k++) {
+                System.out.print(" ");
+            }
+
+            for (int l = 1; l <= i*2; l++) {
+                System.out.print(" ");
+            }
+
+            for (int m = n; m > i; m--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
