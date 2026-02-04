3496670e13b40506573006ae047e02e3ff8018c3
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-04 14:09:01 +0530

    feat: Plus Pattern

--- src/com/java/programming/patterns/PlusPattern.java
+++ src/com/java/programming/patterns/PlusPattern.java
@@ -6,7 +6,7 @@ public class PlusPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number");
+        System.out.println("Enter a number :");
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
