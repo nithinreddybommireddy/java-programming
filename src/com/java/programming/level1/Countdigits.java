1cd206f03f6b9d0a3cc6f75bd75a907705137b3c
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-06 22:50:41 +0530

    feat: Count Digits of a Number

--- src/com/java/programming/level1/Countdigits.java
+++ src/com/java/programming/level1/Countdigits.java
@@ -6,7 +6,7 @@ public class Countdigits {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        System.out.print("enter number:");
+        System.out.print("Enter number:");
         int n=sc.nextInt();
         System.out.println("Digits on given number");
           int count=1;
