96fb0a628edad6415a0a1e43bea0cea9bc2e6909
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-06 22:47:27 +0530

    feat: All Prime number upto N

--- src/com/java/programming/level2/AllPrime1toN.java
+++ src/com/java/programming/level2/AllPrime1toN.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AllPrime1toN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter N: ");
+        System.out.print("Enter Number: ");
         int n = sc.nextInt();
 
         for (int i = 2; i <= n; i++) {
